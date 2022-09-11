5549415f8a0d7b444a1dd649608f38da8daa4170
victorumez <vumezurike@gtlregistrars.com>
2022-09-11 07:00:13 +0100

    created jquery table

--- Web.Client/wwwroot/js/app.js
+++ Web.Client/wwwroot/js/app.js
@@ -1,4 +1,4 @@
-﻿$(document).ready(function () {
-    $('#birth-date').mask('00/00/0000');
-    $('#phone-number').mask('0000-0000');
-})
+﻿
+$(document).ready(function () {
+    $('#todoList').DataTable();
+});
