112954b92cac1ae4a44d89b7eb6c4fd4796a789a
victorumez <vumezurike@gtlregistrars.com>
2022-09-14 16:37:12 +0100

    implemented unit test

---
+++ TodoApiTest/Mocks/MockTodoRepository.cs
@@ -0,0 +1,52 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApiTest.Mocks
+{
+    public static class MockTodoRepository
+    {
+        public static Mock<ITodoRepositoryAsync> GetTodosRepository()
+        {
+            var todos = new List<Todo>
+            {
+                new Todo
+                {
+                    Id = 1,
+                    Title = "test todo",
+                    UserId = "1",
+                    Completed = false,
+                },
+                new Todo
+                {
+                    Id = 2,
+                    Title = "test todo2",
+                    UserId = "2",
+                    Completed = true,
+                }
+            };
+
+            var mockRepo = new Mock<ITodoRepositoryAsync>();  
+            //for get all
+            mockRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(todos);
+            //for add
+            mockRepo.Setup(x => x.AddAsync(It.IsAny<Todo>())).ReturnsAsync((Todo todo) =>
+            {
+                todos.Add(todo);
+                return todo;
+            });
+
+            //update
+            mockRepo.Setup(x => x.UpdateAsync(It.IsAny<Todo>()));
+            //delete
+            mockRepo.Setup(x => x.DeleteAsync(It.IsAny<Todo>()));
+
+            return mockRepo;
+        }
+    }
+}
---
+++ TodoApiTest/Todos/Queries/GetTodosListQueryHandlerTest.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs;
+using Application.Features.Todos.Query;
+using Application.Interfaces.Repositories;
+using Application.Mappings;
+using Application.Wrappers;
+using AutoMapper;
+using Domain.Entities;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoApiTest.Mocks;
+using Xunit;
+using static Application.Features.Todos.Query.GetTodosListQuery;
+
+namespace TodoApiTest.Todos.Queries
+{
+    public class GetTodosListQueryHandlerTest
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<ITodoRepositoryAsync> _mockRepo;
+        public GetTodosListQueryHandlerTest()
+        {
+            _mockRepo = MockTodoRepository.GetTodosRepository();
+
+            //mapping
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<TodoProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+
+        [Fact]
+        public async Task GetTodosListTest()
+        {
+            var handler = new GetTodosListQueryHandler(_mockRepo.Object, _mapper);
+            var result = await handler.Handle(new GetTodosListQuery(), CancellationToken.None);
+            result.ShouldBeOfType<Response<IEnumerable<TodoDto>>>();
+            
+        }
+    }
+}
